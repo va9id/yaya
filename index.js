ac21565a032aa9ede64adb8f3e6d463e84973512
vahido9 <vayeeded@gmail.com>
2024-05-25 12:56:37 -0400

    minor updates

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ onload = () => {
         document.body.classList.remove("not-loaded");
         clearTimeout(c);
     }, 1000);
-
-    /*
-    var h1Element = document.createElement("h1");
-    h1Element.textContent = "Love you";
-    document.body.appendChild(h1Element);
-    */
     const createFooter = () => {
         var footer = document.createElement('footer');
-        footer.textContent = 'Love you';
+        footer.textContent = "Je t'aime";
         document.body.parentNode.insertBefore(footer, document.body.nextSibling);
         setTimeout(function () {
             footer.style.opacity = 1;
