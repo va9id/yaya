70f6ec18566f1fd743eb4cf408cc44fe2caabf69
vahido9 <vayeeded@gmail.com>
2024-05-25 14:01:25 -0400

    minor updates

--- index.js
+++ index.js
@@ -13,4 +13,26 @@ onload = () => {
         }, 100);
     };
     setTimeout(createFooter, 3750)
+
+    /*
+    const stars = () => {
+        let e = document.createElement("div");
+        e.setAttribute("class", "star");
+        document.body.appendChild(e);
+        e.style.left = Math.random() * +innerWidth + "px";
+
+        let size = Math.random() * 12;
+        let duration = Math.random() * 3;
+
+        e.style.fontSize = size + "px";
+        e.style.animationDuration = 2 + duration + "s";
+        setTimeout(function () {
+            document.body.removeChild(e);
+        }, 5000);
+    };
+
+    setInterval(function () {
+        stars();
+    }, 1000);
+    */
 };
